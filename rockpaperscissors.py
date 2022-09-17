c675d36a346866fe0cda570833142aea6ed9ecea
xraycodes <84826985+brianjo94@users.noreply.github.com>
2022-09-16 23:31:22 -0400

    first commit

---
+++ rockpaperscissors.py
@@ -0,0 +1,23 @@
+import random
+
+def play():
+    user = input("r for rock, p for paper, s for scissors ")
+    computer = random.choice(['r', 'p', 's'])
+
+    if user == computer:
+        return "It's a tie"
+    
+    if is_win(user, computer):
+        return "You won"
+
+    return "you lost"
+
+
+   
+def is_win(player, opponent):
+    """# r > s , p > r, s > p"""
+    """return true if player wins"""
+    if (player == 's' and opponent == 'p') or (player == 'r' and opponent == 's') or (player == 'p' and opponent =='r'):
+        return True
+
+print(play())
